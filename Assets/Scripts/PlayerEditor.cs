bcab33df1c6b273ca5537002418a570338c002f0
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2025-03-11 13:10:43 +0000

    fade group - snazzy way to hide fields

--- Assets/Scripts/PlayerEditor.cs
+++ Assets/Scripts/PlayerEditor.cs
@@ -1,17 +1,26 @@
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.AnimatedValues;
+using System;
 
 [CustomEditor(typeof(Player))]
 public class PlayerEditor : Editor
 {
     private SerializedProperty healthProperty;
     private SerializedProperty speedProperty;
-    private int selectedIndex;
+    private int selectedIndex = 0;
+    private AnimBool showExtra;
+
+    string m_String;
+    Color m_Color = Color.white;
+    int m_Number = 0;
 
     private void OnEnable()
     {
         healthProperty = serializedObject.FindProperty("m_health");
         speedProperty = serializedObject.FindProperty("m_moveSpeed");
+        showExtra = new AnimBool(false);
+        showExtra.valueChanged.AddListener(Repaint);
     }
 
     public override void OnInspectorGUI()
@@ -34,7 +43,23 @@ public class PlayerEditor : Editor
             GUILayout.Button("3");
             EditorGUILayout.EndHorizontal();
 
+            showExtra.target = EditorGUILayout.ToggleLeft("Show Extra Fields", showExtra.target);
 
+            if (EditorGUILayout.BeginFadeGroup(showExtra.faded))
+            {
+                EditorGUI.indentLevel++;
+
+                EditorGUILayout.PrefixLabel("Color");
+                m_Color = EditorGUILayout.ColorField(m_Color);
+                EditorGUILayout.PrefixLabel("Text");
+                m_String = EditorGUILayout.TextField(m_String);
+                EditorGUILayout.PrefixLabel("Number");
+                m_Number = EditorGUILayout.IntSlider(m_Number, 0, 10);
+
+                EditorGUI.indentLevel--;
+            }
+
+            EditorGUILayout.EndFadeGroup();
 
             GUILayout.Box("Something");
         }
