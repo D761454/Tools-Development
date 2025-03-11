ed4a589f2ef26292906b50f33eeb12062764a187
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2025-03-11 13:00:41 +0000

    sslection grid for custom and def editors

--- Assets/Scripts/PlayerEditor.cs
+++ Assets/Scripts/PlayerEditor.cs
@@ -18,20 +18,33 @@ public class PlayerEditor : Editor
     {
         serializedObject.Update();
 
-        EditorGUILayout.Slider(healthProperty, 0f, 100f);
-        EditorGUILayout.Slider(speedProperty, 0f, 25f);
-
-        EditorGUILayout.LabelField($"hp - {healthProperty.floatValue}, speed - {speedProperty.floatValue}");
-
-        // button row   -   can also use begin vertical
-        EditorGUILayout.BeginHorizontal();
-        GUILayout.Button("1");
-        GUILayout.Button("2");
-        GUILayout.Button("3");
-        EditorGUILayout.EndHorizontal();
-
         // sets index to value, button remains blue / selected
-        selectedIndex = GUILayout.SelectionGrid(selectedIndex, new string[] { "1..", "2..", "3..", "4.." }, 2);
+        selectedIndex = GUILayout.SelectionGrid(selectedIndex, new string[] { "Custom", "Default"}, 2);
+        if (selectedIndex == 0)
+        {
+            EditorGUILayout.Slider(healthProperty, 0f, 100f);
+            EditorGUILayout.Slider(speedProperty, 0f, 25f);
+
+            EditorGUILayout.LabelField($"hp - {healthProperty.floatValue}, speed - {speedProperty.floatValue}");
+
+            // button row   -   can also use begin vertical
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Button("1");
+            GUILayout.Button("2");
+            GUILayout.Button("3");
+            EditorGUILayout.EndHorizontal();
+
+
+
+            GUILayout.Box("Something");
+        }
+        else
+        {
+            this.DrawDefaultInspector();
+        }
+        
+
+
 
         serializedObject.ApplyModifiedProperties();
     }
