5132fc261019926eac904238e3d54efd0b2951cf
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2025-03-17 12:39:56 +0000

    WIP scene handle

--- Assets/Scripts/OBJPlacerEditorWindow.cs
+++ Assets/Scripts/OBJPlacerEditorWindow.cs
@@ -1,26 +1,44 @@
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
+using UnityEditor.UIElements;
 
 public class OBJPlacerEditorWindow : EditorWindow
 {
-    private int genWidth;
-    private int genHeight;
+    [SerializeField] private int genWidth;
+    [SerializeField] private int genHeight;
+    [SerializeField] private static float brushSize = 50f;
 
     [MenuItem("OBJ Placement/Placement Tool")]
     public static void ShowWindow()
     {
         OBJPlacerEditorWindow window = GetWindow<OBJPlacerEditorWindow>();
         window.titleContent = new GUIContent("Placement Tool");
+        SceneView.duringSceneGui -= OnSceneGUI;
+        SceneView.duringSceneGui += OnSceneGUI;
     }
 
     public void CreateGUI()
     {
         VisualElement root = new VisualElement();
 
-        VisualTreeAsset asset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UXML/terrainGeneratorEditor.uxml");
+        VisualTreeAsset asset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UXML/OBJPlacementMainEditor.uxml");
         asset.CloneTree(root);
 
+        root.Bind();
+
+        StyleSheet sheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/USS/OBJPlacementMainEditor.uss");
+        root.styleSheets.Add(sheet);
+
         rootVisualElement.Add(root);
+    }
+
+    static void OnSceneGUI(SceneView sceneView)
+    {
+        //https://discussions.unity.com/t/how-to-get-mouseposition-in-scene-view/519147
+
+        Handles.BeginGUI();
+        Handles.DrawWireDisc(Event.current.mousePosition, Vector3.forward, brushSize);
+        Handles.EndGUI();
     }
 }
