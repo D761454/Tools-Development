c8d1ea7c41ec13503bb2dca963dd92fe26eff416
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2025-03-21 16:33:06 +0000

    initialise brush editor tool data from serialised obj

--- Assets/Scripts/OBJPlacerEditorTool.cs
+++ Assets/Scripts/OBJPlacerEditorTool.cs
@@ -11,38 +11,108 @@ using UnityEditor.ShortcutManagement;
 [EditorTool("OBJ Brush")]
 public class OBJPlacerEditorTool : EditorTool, IDrawSelectedHandles
 {
+    private static OBJPlacerScriptableOBJ serializedClass;
+    private SerializedObject serializedObject;
+
+    [SerializeField] VisualTreeAsset visualTree;
+    [SerializeField] VisualTreeAsset groupTree;
+
     public float brushSize = 100f;
+    public bool brushEnabled = false;
+    public int density = 50;
+    public List<GroupStruct> groups;
+    public GameObject tempObj;
+    public int tempWeight = 50;
 
     void OnEnable()
     {
+        serializedClass = AssetDatabase.LoadAssetAtPath<OBJPlacerScriptableOBJ>("Assets/Scripts/OBJ Placer Scriptable OBJ.asset");
 
+        if (!serializedClass)
+        {
+            serializedClass = CreateInstance<OBJPlacerScriptableOBJ>();
+            AssetDatabase.CreateAsset(serializedClass, "Assets/Scripts/OBJ Placer Scriptable OBJ.asset");
+            AssetDatabase.Refresh();
+        }
+
+        brushSize = serializedClass.brushSize;
+        brushEnabled = serializedClass.brushEnabled;
+        density = serializedClass.density;
+        groups = serializedClass.groups;
+        tempObj = serializedClass.tempObj;
+        tempWeight = serializedClass.tempWeight;
     }
 
     void OnDisable()
     {
-
+        
     }
 
     // Called when the active tool is set to this tool instance.
     public override void OnActivated()
     {
         SceneView.lastActiveSceneView.ShowNotification(new GUIContent("Entering OBJ Brush Tool"), .1f);
+        SceneVisibilityManager.instance.DisableAllPicking();
     }
 
     // Called before the active tool is changed, or destroyed.
     public override void OnWillBeDeactivated()
     {
         SceneView.lastActiveSceneView.ShowNotification(new GUIContent("Exiting OBJ Brush Tool"), .1f);
+        SceneVisibilityManager.instance.EnableAllPicking();
     }
 
     // Equivalent to Editor.OnSceneGUI.
     public override void OnToolGUI(EditorWindow window)
     {
-        Handles.BeginGUI();
         Event e = Event.current;
+
+        Handles.color = Color.yellow;
+
         Vector3 mousePosition = e.mousePosition;
-        Handles.DrawWireDisc(mousePosition, Vector3.forward, brushSize);
-        Handles.EndGUI();
+
+        Ray ray = HandleUtility.GUIPointToWorldRay(mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Handles.BeginGUI();
+            if (brushEnabled)
+            {
+                Handles.DrawWireDisc(mousePosition, Vector3.forward, brushSize);
+                if (tempObj != null && e.type == EventType.MouseDown && e.button == 0)
+                {
+                    int rand = UnityEngine.Random.Range(0, 100);
+                    if (rand < tempWeight)
+                    {
+                        var obj = PrefabUtility.InstantiatePrefab(tempObj);
+                        SceneVisibilityManager.instance.DisablePicking((GameObject)obj, false);
+                        Vector3 spawnPos = hit.point; spawnPos.y += obj.GetComponent<Renderer>().bounds.size.y / 2;
+                        Vector3 surfaceNormal = hit.normal.normalized;
+
+                        obj.GetComponent<Transform>().position = spawnPos;
+                        obj.GetComponent<Transform>().rotation = Quaternion.FromToRotation(obj.GetComponent<Transform>().up, surfaceNormal);
+
+                        // note: do pos after rotation to move obj along surface normal instead of world y
+                    }
+                }
+            }
+            Handles.EndGUI();
+        }
+
+        // update info on gui changes
+        if (EditorWindow.HasOpenInstances<OBJPlacerEditorWindow>())
+        {
+            var toolWindow = EditorWindow.GetWindow<OBJPlacerEditorWindow>();
+
+            // change to serialised obj
+            brushSize = serializedClass.brushSize;
+            brushEnabled = serializedClass.brushEnabled;
+            density = serializedClass.density;
+            groups = serializedClass.groups;
+            tempObj = serializedClass.tempObj;
+            tempWeight = serializedClass.tempWeight;
+        }
     }
 
     public void OnDrawHandles()
--- Assets/Scripts/OBJPlacerEditorWindow.cs
+++ Assets/Scripts/OBJPlacerEditorWindow.cs
@@ -16,13 +16,6 @@ public class OBJPlacerEditorWindow : EditorWindow
     [SerializeField] VisualTreeAsset visualTree;
     [SerializeField] VisualTreeAsset groupTree;
 
-    public float brushSize = 100f;
-    public bool brushEnabled = false;
-    public int density = 50;
-    public List<GroupStruct> groups;
-    public GameObject tempObj;
-    public int tempWeight = 50;
-
     [MenuItem("OBJ Placement/Placement Tool")]
     public static void Init()
     {
@@ -41,18 +34,6 @@ public class OBJPlacerEditorWindow : EditorWindow
             AssetDatabase.CreateAsset(serializedClass, "Assets/Scripts/OBJ Placer Scriptable OBJ.asset");
             AssetDatabase.Refresh();
         }
-    }
-
-    private void OnEnable()
-    {
-        SceneVisibilityManager.instance.DisableAllPicking();
-        SceneView.duringSceneGui += OnSceneGUI;
-    }
-
-    private void OnDisable()
-    {
-        SceneVisibilityManager.instance.EnableAllPicking();
-        SceneView.duringSceneGui -= OnSceneGUI;
     }
 
     // generate GUI if no editor updates
@@ -74,55 +55,11 @@ public class OBJPlacerEditorWindow : EditorWindow
         rootVisualElement.Add(root);
     }
 
-    void OnSceneGUI(SceneView sceneView)
-    {
-        Event e = Event.current;
-
-        Handles.color = Color.yellow;
-
-        Vector3 mousePosition = e.mousePosition;
-
-        Ray ray = HandleUtility.GUIPointToWorldRay(mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, 100))
-        {
-            //Handles.BeginGUI();
-            if (brushEnabled)
-            {
-                //Handles.DrawWireDisc(mousePosition, Vector3.forward, brushSize);
-                if (tempObj != null && e.type == EventType.MouseDown && e.button == 0)
-                {
-                    int rand = UnityEngine.Random.Range(0, 100);
-                    if (rand < tempWeight)
-                    {
-                        var obj = PrefabUtility.InstantiatePrefab(tempObj);
-                        SceneVisibilityManager.instance.DisablePicking((GameObject)obj, false);
-                        Vector3 spawnPos = hit.point; spawnPos.y += obj.GetComponent<Renderer>().bounds.size.y / 2;
-                        Vector3 surfaceNormal = hit.normal.normalized;
-
-                        obj.GetComponent<Transform>().position = spawnPos;
-                        obj.GetComponent<Transform>().rotation = Quaternion.FromToRotation(obj.GetComponent<Transform>().up, surfaceNormal);
-
-                        // note: do pos after rotation to move obj along surface normal instead of world y
-                    }
-                }
-            }
-            //Handles.EndGUI();
-        }
-    }
-
     // handle gui events
     private void OnGUI()
     {
         serializedObject = new SerializedObject(serializedClass);
         serializedObject.Update();
-
-        // get UI data - two way via bindings so dont need to set serialized object data
-        brushSize = rootVisualElement.Q<Slider>(name: "bSize").value;
-        brushEnabled = rootVisualElement.Q<Toggle>(name: "bToggle").value;
-        tempObj = (GameObject)rootVisualElement.Q<ObjectField>().value;
-        tempWeight = rootVisualElement.Q<SliderInt>(name: "tWeight").value;
 
         serializedObject.ApplyModifiedProperties();
     }
